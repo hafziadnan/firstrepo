ec5def20ae9981998a5a04021b5a6b0da58bf621
1ST-HTML1 <hafizmadnan567@gmail.com>
2024-06-16 16:52:22 +0500

    my first commint

---
+++ 1_basic.py
@@ -0,0 +1,19 @@
+# 5+7
+# print("I am a student of bscs.ok")
+# print(2+3)
+# a = "pakistan"
+# b= "ali"
+# print(a + b )
+# x=22
+# y=22
+# print(x+y)
+person_A_name = input("What is your name:")
+person_A_age = input("Plz enter your age:")
+
+person_B_name = input("What is your name:")
+person_B_age = input("Plz enter your age:")
+
+if person_A_age > person_B_age:
+    print(person_A_name,"is younger",person_B_name)
+else:
+    print(person_A_name,"is older than",person_B_name)
